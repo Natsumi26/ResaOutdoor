f1a482deecaf86d1d948da0a6511ac8643cd5cfc
Ubuntu <ubuntu@vps-78ec9692.vps.ovh.net>
2025-11-20 09:18:39 +0000

    Fix stripe config + update package.json

--- backend/src/config/stripe.js
+++ backend/src/config/stripe.js
@@ -1,8 +1,6 @@
 import Stripe from 'stripe';
 
 // Initialiser Stripe avec la clé secrète
-const stripe = new Stripe(process.env.STRIPE_SECRET_KEY, {
-  apiVersion: '2023-10-16'
-});
+const stripe = new Stripe('sk_test_51SKEHQFJHhOlwSPt3szTIHWUcQYg2e8PUiRzDc5lmrtUzqDdDzNZPJ6dM8UaNn1HdRkoiq9G0bEJWVj6WTkAmFGO00jYf8sLnc');
 
 export default stripe;
